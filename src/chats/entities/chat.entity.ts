88d5a76b291d8926216639fd87cefd53df538b38
matheus.araujo <matheus.araujo@trinusco.com.br>
2024-11-18 20:01:03 -0300

    firts commit

---
+++ src/chats/entities/chat.entity.ts
@@ -0,0 +1,30 @@
+import { ObjectType, Field } from '@nestjs/graphql';
+import { AbstractEntity } from '../../common/database/abstract.entity';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Message } from '../messages/entities/message.entity';
+
+@ObjectType()
+@Schema()
+export class Chat extends AbstractEntity {
+  @Field()
+  @Prop()
+  userId: string;
+
+  @Field()
+  @Prop()
+  isPrivate: boolean;
+
+  @Field(() => [String])
+  @Prop([String])
+  userIds: string[];
+
+  @Field({ nullable: true })
+  @Prop()
+  name?: string;
+
+  @Prop([Message])
+  messages: Message[];
+
+}
+
+export const ChatSchema = SchemaFactory.createForClass(Chat);
